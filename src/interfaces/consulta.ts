04ce73c0f405571fd70e021c557fcf0ba049d019
viniciuscont2 <vinicont39@gmail.com>
2026-04-28 09:07:07 -0300

    Commit - CP02

--- src/interfaces/consulta.ts
+++ src/interfaces/consulta.ts
@@ -1,6 +1,7 @@
 import { Medico } from "./medico";
 import { Paciente } from "../types/paciente";
 import { StatusConsulta } from "../types/statusConsulta";
+
 export interface Consulta {
   id: number;
   medico: Medico;
